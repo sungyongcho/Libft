cd95ecbad850d72a642d374c2ec44f28bed68407
Sungyong Cho <here@sungyongcho.com>
2023-08-05 01:56:24 +0200

    feat: ft_itoa updated

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,42 +1,63 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sucho <sucho@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/04/26 00:42:17 by sucho             #+#    #+#             */
-/*   Updated: 2023/08/04 22:54:11 by sucho            ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   ft_itoa.c										  :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: sucho <sucho@student.42.fr>				+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2020/04/26 00:42:17 by sucho			 #+#	#+#			 */
+/*   Updated: 2023/08/05 01:52:51 by sucho			###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char			*ft_itoa(int n)
+static int	ft_get_digits(int n)
 {
-	long		value;
-	size_t		count;
-	char		*result;
+	int				i;
+	unsigned int	un;
 
-	value = n;
-	if ((count = 1) && value < 0)
-		count++;
-	if (value < 0)
-		value *= -1;
-	while ((value /= 10) > 0)
-		count++;
-	if (!(result = (char *)malloc(sizeof(char) * (count + 1))))
-		return (NULL);
-	value = (long)n * 10;
-	if (value < 0)
+	i = 1;
+	if (n < 0)
 	{
-		result[0] = '-';
-		value *= -1;
+		un = (unsigned int)-n;
+		i++;
 	}
-	result[count--] = '\0';
-	if (value == 0)
-		result[count] = '0';
-	while ((value /= 10) > 0)
-		result[count--] = value % 10 + '0';
-	return (result);
+	else
+		un = (unsigned int)n;
+	while (un >= 10)
+	{
+		un /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char			*str;
+	int				len;
+	unsigned int	un;
+
+	len = ft_get_digits(n);
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	if (n < 0)
+	{
+		str[0] = '-';
+		un = (unsigned int)-n;
+	}
+	else
+		un = (unsigned int)n;
+	str[len] = '\0';
+	while (len-- > 0)
+	{
+		if (str[len] != '-')
+		{
+			str[len] = un % 10 + '0';
+			un /= 10;
+		}
+	}
+	return (str);
 }
